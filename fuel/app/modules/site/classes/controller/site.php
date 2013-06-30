cbde4f6da4141c6c6ef6b8e7fa6022e33602cb2d
bmartel <brandonmartel@gmail.com>
2013-06-29 23:06:31 -0500

    Inital Commit

---
+++ fuel/app/modules/site/classes/controller/site.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.6
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status. Using modules/HMVC structure.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+namespace Site;
+
+class Controller_Site extends \Controller
+{
+
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		return \Response::forge(\View::forge('site/index'));
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
+	 * show how to use them.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return \Response::forge(\ViewModel::forge('site/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return \Response::forge(\ViewModel::forge('site/404'), 404);
+	}
+}
---
+++ fuel/app/modules/site/classes/view/site/hello.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.6
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The welcome hello view model.
+ *
+ * @package  app
+ * @extends  ViewModel
+ */
+namespace Site;
+
+class View_Site_Hello extends \ViewModel
+{
+	/**
+	 * Prepare the view data, keeping this in here helps clean up
+	 * the controller.
+	 *
+	 * @return void
+	 */
+	public function view()
+	{
+		$this->name = $this->request()->param('name', 'World');
+	}
+}
